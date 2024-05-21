e2819a54149bd5e290dc04e35ad26d078b4afd32
Jleagle <jimeagle@gmail.com>
2024-05-21 17:33:22 +0100

    Fix index

--- storage/mysql/schema.sql
+++ storage/mysql/schema.sql
@@ -57,12 +57,10 @@ CREATE TABLE rubix.`user_roles` (
     `workspace` varchar(64) NOT NULL,
     `user`      varchar(64) NOT NULL,
     `role`      varchar(64) NOT NULL,
-    PRIMARY KEY (`workspace`, `user`, `role`),
-    constraint role_users
-        unique (`workspace`, `role`)
+    PRIMARY KEY (`workspace`, `user`, `role`)
 );
 
-
+create index role_users on rubix.`user_roles` (workspace, role);
 
 CREATE TABLE rubix.`user_status` (
     `workspace`     varchar(64) NOT NULL,
