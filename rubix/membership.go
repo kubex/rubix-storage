e3829efc7d33bbc38f93037edfc78e36da6a2aeb
Jleagle <jimeagle@gmail.com>
2024-05-30 14:54:59 +0100

    Render types nicely

--- rubix/membership.go
+++ rubix/membership.go
@@ -1,8 +1,17 @@
 package rubix
 
-import "time"
+import (
+	"time"
+
+	"golang.org/x/text/cases"
+	"golang.org/x/text/language"
+)
 
 type MembershipType string
+
+func (us UserState) String() string {
+	return cases.Title(language.English, cases.Compact).String(string(us))
+}
 
 const (
 	MembershipTypeOwner   MembershipType = "owner"   // Full access
@@ -11,6 +20,23 @@ const (
 )
 
 type MembershipState int
+
+func (us MembershipState) String() string {
+	switch us {
+	case MembershipStatePending:
+		return "Pending"
+	case MembershipStateActive:
+		return "Active"
+	case MembershipStateSuspended:
+		return "Suspended"
+	case MembershipStateArchived:
+		return "Archived"
+	case MembershipStateRemoved:
+		return "Removed"
+	default:
+		return "Unknown"
+	}
+}
 
 const (
 	MembershipStatePending   MembershipState = 0
